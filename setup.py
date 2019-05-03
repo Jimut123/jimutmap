2b544b5da8ae4cd238d88f0aab006de4b0b94eeb
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2019-05-04 00:19:39 +0530

    jmaps added

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jmap",
+    version="0.0.1",
+    author="Jimut Bahan Pal",
+    author_email="jimutbahanpal@yahoo.com",
+    description="To get enormous amount of Apple Maps tile with ease",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Jimut123/jmap",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
