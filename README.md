cf29e8bf8a33a582ec9f21c357a529ae7563291a
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2019-05-04 00:31:07 +0530

    jimutmap

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# jmap
+# jimutmap
 
 #### Purpose 
 
--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="jmap",
+    name="jimutmap",
     version="0.0.1",
     author="Jimut Bahan Pal",
     author_email="jimutbahanpal@yahoo.com",
     description="To get enormous amount of Apple Maps tile with ease",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Jimut123/jmap",
+    url="https://github.com/Jimut123/jimutmap",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
