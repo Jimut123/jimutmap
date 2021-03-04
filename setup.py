0225b5aa6da32f869df8a36fce2987d21f4d63c5
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2021-03-04 09:31:41 +0530

    @tigerhawkvok, major bug fixes and optimised code

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jimutmap",
-    version="1.3.7",
+    version="1.3.8",
     author="Jimutmap Contributors",
     author_email="jimutbahanpal@yahoo.com",
     description="To get enormous amount of Apple Maps tile with ease",
