5e1269868d2e502f0e42ead18f2070d2a0766ef2
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2021-03-25 21:52:35 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Jimut123/jimutmap",
-    install_requires=['wget','bs4','numpy','tqdm'],
+    install_requires=['certifi==2020.12.5','chardet==4.0.0','chromedriver-autoinstaller==0.2.2','idna==2.10',
+                      'numpy==1.19.5','requests==2.25.1','selenium==3.141.0','tqdm==4.53.0','urllib3==1.26.3'],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
