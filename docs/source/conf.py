6506b00fb46ae8e3d4f338e0d050bb4712a58d37
Jimut123 <jimutbahanpal@yahoo.com>
2021-04-30 14:24:48 +0530

    sphinx theme added

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@
 from __future__ import absolute_import
 from docutils import nodes
 from docutils.parsers.rst import Directive, directives
-
+import sphinx_rtd_theme
 
 
 def align(argument):
@@ -142,7 +142,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -245,6 +245,11 @@ epub_exclude_files = ['search.html']
 
 
 # -- Extension configuration -------------------------------------------------
+
+extensions = [
+    
+    "sphinx_rtd_theme",
+]
 
 # -- Options for intersphinx extension ---------------------------------------
 
