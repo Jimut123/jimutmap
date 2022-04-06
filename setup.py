0a483b5f3bdc55da7b2eed350bea4f0ebff8a77e
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2022-04-06 19:09:25 +0530

    add

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jimutmap",
-    version="1.4.1",
+    version="1.4.1.2",
     author="Jimut Bahan Pal and the Jimutmap Contributors",
     author_email="jimutbahanpal@yahoo.com",
     description="To get enormous amount of map tiles with ease",
@@ -25,7 +25,7 @@ setuptools.setup(
     url="https://github.com/Jimut123/jimutmap",
     install_requires=['certifi==2020.12.5','chardet==4.0.0','chromedriver-autoinstaller==0.2.2','idna==2.10',
                       'numpy==1.19.5','requests==2.25.1','selenium==3.141.0','tqdm==4.53.0','urllib3==1.26.5', 
-                      'opencv-python==4.5.5'],
+                      'opencv-python==4.5.5.64'],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
